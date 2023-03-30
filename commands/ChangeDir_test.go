0bbe79e0a27b7f59034b69ba8636c1712611ef87
Wen Junhua <wenjh1998@gmail.com>
2023-03-31 02:50:03 +0800

    feat: add test for new chdir command

--- commands/ChangeDir_test.go
+++ commands/ChangeDir_test.go
@@ -5,6 +5,34 @@ import (
 	"testing"
 )
 
+func TestNewChangeDirectoryCommand(t *testing.T) {
+	// Struct for the tests
+	type args struct {
+		args []string
+		want Command
+	}
+
+	// Create the tests
+	tests := []args{
+		{
+			args: []string{".."},
+			want: &ChangeDirectoryCommand{},
+		},
+	}
+
+	// Run the tests
+	for _, tt := range tests {
+		t.Run(
+			"TestNewChangeDirectoryCommand",
+			func(t *testing.T) {
+				if got := NewChangeDirectoryCommand(tt.args); got == nil {
+					t.Errorf("NewChangeDirectoryCommand() returned nil")
+				}
+			},
+		)
+	}
+}
+
 func TestExecuteChangeDir(t *testing.T) {
 	// Save current dir
 	cwd, err := os.Getwd()
