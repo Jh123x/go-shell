08106e9ab71b535bd8ec29566ee496240d8c2ab8
Wen Junhua <wenjh1998@gmail.com>
2023-03-31 01:53:40 +0800

    feat: add command manager test

--- commands/CommandManager_test.go
+++ commands/CommandManager_test.go
@@ -1,10 +1,11 @@
 package commands
 
 import (
+	"reflect"
 	"testing"
 )
 
-// TestNewCommandManager tests the NewCommandManager function
+// Create tests
 func TestNewCommandManager(t *testing.T) {
 	cm := NewCommandManager()
 	if cm == nil {
@@ -12,13 +13,43 @@ func TestNewCommandManager(t *testing.T) {
 	}
 }
 
-// TestGetCommand tests the GetCommand function
 func TestGetCommand(t *testing.T) {
+
+	// Struct for the tests
+	type args struct {
+		name string
+		arg  string
+		want Command
+	}
+
+	emptyArgs := []string{""}
+	tests := make([]args, 0, len(mappedCommands))
 	cm := NewCommandManager()
+
+	// Create the tests
 	for cmd, _ := range mappedCommands {
-		_, ok := cm.GetCommand(cmd)
-		if !ok {
-			t.Errorf("GetCommand(%s) returned false", cmd)
-		}
+		tests = append(tests, args{
+			name: cmd,
+			arg:  cmd,
+			want: mappedCommands[cmd](emptyArgs),
+		})
+	}
+
+	// Run the tests
+	for _, tt := range tests {
+		t.Run(
+			tt.name,
+			func(t *testing.T) {
+				if cmd, ok := cm.GetCommand(tt.name); cmd == nil || !ok {
+					t.Errorf("GetCommand() returned nil or false")
+				} else {
+					result := cmd(emptyArgs)
+					// Check if commands are the same type
+					if reflect.TypeOf(result) != reflect.TypeOf(tt.want) {
+						t.Errorf("GetCommand() returned wrong command type")
+					}
+				}
+			},
+		)
 	}
 }
